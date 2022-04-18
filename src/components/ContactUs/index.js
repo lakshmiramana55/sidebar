e2a58fef3c9cc3fd15fc7376d865bf7b1b2d5e38
ramana55 <lakshmiramana756@gmail.com>
2022-04-18 09:48:20 +0000

    commit message

---
+++ src/components/ContactUs/index.js
@@ -0,0 +1,10 @@
+const ContactUs = () => (
+  <>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-watch-facebook-logo-img.png "
+      alt="facebook logo"
+    />
+    <h1>CONTACT US</h1>
+  </>
+)
+export default ContactUs
